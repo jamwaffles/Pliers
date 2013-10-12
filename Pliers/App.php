12703c81943f158e0374c1ba6731eb817fa8ee3b
James Waples <jamwaffles@gmail.com>
2013-10-12 20:20:56 +0100

    Added Redbean setup code, as well as code to load config from a config.json file

--- Pliers/App.php
+++ Pliers/App.php
@@ -1,11 +1,21 @@
 <?php
 namespace Pliers;
 
+use RedBean_Facade as R;
+
 class App extends \Slim\Slim {
+	protected $conf;
+
 	public function __construct() {
 		parent::__construct(array(
 			'templates.path' => realpath('../views')
 		));
+
+		$this->setupConfig();
+
+		// Set up RedBean
+		R::setup($this->conf->db->dsn, $this->conf->db->username, $this->conf->db->password);
+		R::freeze(true);		// Don't allow modifications to the DB schema
 
 		$this->router = new \Pliers\Router;
 
@@ -22,12 +32,39 @@ class App extends \Slim\Slim {
 		)));
 	}
 
+	protected function appRoot() {
+		return realpath($this->root() . '../');
+	}
+
 	public function start() {
 		$this->router->start();
 	}
 
 	public function mode() {
 		return $this->mode;
+	}
+
+	private function setupConfig() {
+		$confFile = $this->appRoot() . '/config.json';
+
+		$file = file_get_contents($confFile);
+
+		if(!$file) {
+			throw new \Slim\Exception\Stop('No configuration file found. Add config.json to your app root.');
+
+			$app->halt(500);
+		}
+
+		$mode = $this->getMode();
+		$conf = json_decode($file);
+
+		if(!$conf->$mode) {
+			throw new \Slim\Exception\Stop('No configuration found for mode ' . $mode . '. Check your configuration.');
+
+			$app->halt(500);
+		}
+		
+		$this->conf = $conf->$mode;
 	}
 }
 ?>
